3dfb90d52ec5ea13ea83bc5e48607c11e60204ad
pnrmaja <ponauerszilvi1@gmail.com>
2026-09-18 15:38:40 +0200

    gender

---
+++ movies/app/Models/Gender.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+#[Fillable(['title'])]
+
+class Gender extends Model
+{
+    /** @use HasFactory<\Database\Factories\GenderFactory> */
+    use HasFactory;
+}
